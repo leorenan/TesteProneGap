a5e7cd36ef4cf1b3fdb3e80c4c450505e4b22c45
leorenan <leo_renan@outlook.com>
2014-09-12 22:35:31 -0300

    agoravai

--- aulapp/www/js/index.js
+++ aulapp/www/js/index.js
@@ -27,6 +27,7 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+		document.getElementById('scan').addEventListener('click', this.scan, false);
     },
     // deviceready Event Handler
     //
